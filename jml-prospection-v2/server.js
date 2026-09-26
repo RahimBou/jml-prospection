8481db19741e2c7263079caa3e77525055d5b06a
RahimBou <92314089+RahimBou@users.noreply.github.com>
2026-09-26 17:25:18 +0200

    chore: expose version 2.4.2

--- jml-prospection-v2/server.js
+++ jml-prospection-v2/server.js
@@ -6,7 +6,7 @@ const { searchPublicListings, geocodeAddress } = require("./src/sources");
 const { buildMarketSnapshot } = require("./src/market-engine");
 const { buildSectorRadar } = require("./src/sector-radar");
 
-const VERSION = "2.4.1";
+const VERSION = "2.4.2";
 const PORT = Number(process.env.PORT || 10000);
 const ROOT = __dirname;
 const PUBLIC = path.join(ROOT, "public");
