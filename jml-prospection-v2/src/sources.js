664860f18d672867b81333adbf2a94a86c9c5802
RahimBou <92314089+RahimBou@users.noreply.github.com>
2026-09-26 16:50:32 +0200

    Version sources V2.3.2

--- jml-prospection-v2/src/sources.js
+++ jml-prospection-v2/src/sources.js
@@ -198,7 +198,7 @@ async function searchPublicListings(params = {}) {
   });
   return {
     source: "Web public local",
-    version: "2.3.1",
+    version: "2.3.2",
     total: unique.length,
     items: unique,
     sources: sites.map((site, i) => ({
